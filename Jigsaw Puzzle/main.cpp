c27e969e8c76fa2380571d14bba2a9669576517f
dong128 <dong128@72394ba8-1b3d-11de-a78b-a7aca27b2395>
2009-04-22 09:03:08 +0000

    There is some problem...

--- Jigsaw Puzzle/main.cpp
+++ Jigsaw Puzzle/main.cpp
@@ -5,118 +5,107 @@
 
 using namespace std;
 
-pair<int, int> encode_state(string state, char jigsaw[3][3]) {
-    int i0, j0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            jigsaw[i][j] = state[i * 3 + j];
-            if (jigsaw[i][j] == '0') {
-                i0 = i; j0 = j;
-            }
-        }
-    }
-    return make_pair(i0, j0);
-}
+typedef string Jigsaw;
 
-string code_state(char jigsaw[3][3]) {
-    string state;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            state.push_back(jigsaw[i][j]);
-        }
-    }
-    //cout << '\t' << state << endl;
-    return state;
-}
+const Jigsaw st_state = "012345678";
+const int fac[9] = {0, 1, 2, 6, 24, 120, 720, 5040, 40320};
 
-string ch_state(string state, int ch) {
-    char jigsaw[3][3];
-    pair<int, int> coodr = encode_state(state, jigsaw);
-    char t;
+bool ch_state(Jigsaw& jigsaw, int ch) {
+
+    int i0;
+    for (i0 = 0; i0 < 9; i0++)
+        if (jigsaw[i0] == '0') break;
+
     bool flag = false;
     switch (ch) {
         case 0:
-            if (coodr.second > 0) {
-                t = jigsaw[coodr.first][coodr.second];
-                jigsaw[coodr.first][coodr.second] = jigsaw[coodr.first][coodr.second-1];
-                jigsaw[coodr.first][coodr.second-1] = t;
+            if (i0 - 3 > 0) {
+                jigsaw[i0] = jigsaw[i0-3];
+                jigsaw[i0-3] = '0';
                 flag = true;
             }
             break;
         case 1:
-            if (coodr.first < 2) {
-                t = jigsaw[coodr.first][coodr.second];
-                jigsaw[coodr.first][coodr.second] = jigsaw[coodr.first+1][coodr.second];
-                jigsaw[coodr.first+1][coodr.second] = t;
+            if (i0 % 3 < 2) {
+                jigsaw[i0] = jigsaw[i0+1];
+                jigsaw[i0+1] = '0';
                 flag = true;
             }
             break;
         case 2:
-            if (coodr.second < 2) {
-                t = jigsaw[coodr.first][coodr.second];
-                jigsaw[coodr.first][coodr.second] = jigsaw[coodr.first][coodr.second+1];
-                jigsaw[coodr.first][coodr.second+1] = t;
+            if (i0 + 3 < 9) {
+                jigsaw[i0] = jigsaw[i0+3];
+                jigsaw[i0+3] = '0';
                 flag = true;
             }
             break;
         case 3:
-            if (coodr.first > 0) {
-                t = jigsaw[coodr.first][coodr.second];
-                jigsaw[coodr.first][coodr.second] = jigsaw[coodr.first-1][coodr.second];
-                jigsaw[coodr.first-1][coodr.second] = t;
+            if (i0 % 3 > 0) {
+                jigsaw[i0] = jigsaw[i0-1];
+                jigsaw[i0-1] = '0';
                 flag = true;
             }
             break;
     }
-    if (flag) return code_state(jigsaw);
-    else return state;
+    return flag;
 }
 
-int chg_int(string str) {
-    int r = 0;
-    for (int i = 0; i < str.size(); i++) {
-        r *= 10;
-        r += (char)str[i]-48;
+int hashcode(Jigsaw jigsaw) {
+    bool app_flag[9] = {false};
+    int i, j;
+    int code = 0;
+    for (i = 0; i < 8; i++) {
+        j = jigsaw[i] - 48;
+        app_flag[j--] = true;
+        int k = 0;
+        for (; j >= 0; j--) {
+            if (!app_flag[j]) k++;
+        }
+        code += k * fac[8-i];
     }
+    return code;
 }
 
-set<string> state_hash[1000000];
+int state_hash[362880];
 
 int main() {
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
 
-    const string st_state = "012345678";
-    char tmp_str[10];
-    string fi_state;
+    memset(state_hash, -1, sizeof(state_hash));
 
-    while (scanf("%s", &tmp_str) != EOF) {
-        //printf("%s\n", tmp_str);
-        fi_state = tmp_str;
-        set<string> state_set;
-        queue<string> state_que;
-        //state_set.insert(st_state);
-        state_hash[12345].insert("678");
-        state_que.push(st_state);
-        int step = 0;
-        for (int i = 0; i < 1000000; i++) state_hash[i].clear();
-        while (state_que.size() != 0) {
-            string cur_state = state_que.front();
-            //cout << cur_state << '-' << fi_state << endl;
-            if (cur_state == fi_state) break;
-            state_que.pop();
-            for (int k = 0; k < 4; k++) {
-                string new_state = ch_state(cur_state, k);
-                if (new_state != cur_state) {
-                    if (state_hash[chg_int(new_state.substr(0, 6))].insert(new_state.substr(6, 3)).second) {
-                        state_que.push(new_state);
-                    }
+    queue<pair<Jigsaw, int> > state_que;
+    state_hash[0] = 0;
+    state_que.push(make_pair(st_state, 0));
+
+    while (state_que.size() != 0) {
+        Jigsaw cur_state = state_que.front().first;
+        int step = state_que.front().second;
+        state_que.pop();
+        step++;
+        for (int k = 0; k < 4; k++) {
+            Jigsaw new_state = cur_state;
+            if (ch_state(new_state, k)) {
+                int code = hashcode(new_state);
+
+                if (state_hash[code] < 0) {
+                    //cout << cur_state << "->" << new_state << ':' << step << endl;
+                    state_hash[code] = step;
+                    state_que.push(make_pair(new_state, step));
                 }
             }
-            //cout << state_que.size() << endl;
-            step++;
         }
-        if (state_que.size() == 0) printf("NO\n");
+        //if (step > 6) break;
+    }
+
+    Jigsaw fi_state;
+    char tmp_str[10];
+    while (scanf("%s", &tmp_str) != EOF) {
+        fi_state = tmp_str;
+        int code = hashcode(fi_state);
+        //printf("%d\n", code);
+        int step = state_hash[code];
+        if (step < 0) printf("NO\n");
         else printf("%d\n", step);
     }
     return 0;
